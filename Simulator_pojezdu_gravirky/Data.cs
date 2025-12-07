67936bdc3bba00fe6dfb89cf8b08161e8c5c3893
josef-kyvala-vut <267490@vutbr.cz>
2025-12-07 17:10:03 +0100

    verze 1 - zakladni struktura a absolutini a relativni pohyb

---
+++ Simulator_pojezdu_gravirky/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator_pojezdu_gravirky
+{
+    public struct Poloha
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        public Poloha(double polohaX, double polohaY)
+        {
+            X = polohaX;
+            Y = polohaY;
+        }
+
+        public override string ToString()
+        {
+            return $"[{X:F1}; {Y:F1}]";
+        }
+    }
+}
+
